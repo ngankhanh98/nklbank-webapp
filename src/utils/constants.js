3fd8a54b5a671ea6475f2264790e1752f8258bd5
ngankhanh98 <ngankhanh98@gmail.com>
2020-08-26 17:06:19 +0700

    :sparkles: auth

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const OAuthTypes = {
+  OAUTH_ACCESS_APPROVED: "oauth_access_approved",
+  OAUTH_ACCESS_DENIED: "oauth_access_denied",
+  OAUTH_MISSING_CODE: "oauth_missing_code",
+  OAUTH_INVALID_PARAM: "oauth_invalid_param",
+};
+
+export const ActionTypes = {
+    GET_INFORMATION: 'get_information'
+}
